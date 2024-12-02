89fcf335aed711ad47c39dc9391263b36d3e92dd
Rob Burgess <rob.burgess@live.co.uk>
2024-12-02 08:27:31 +0000

    small day 2 refactor

--- day2/index.ts
+++ day2/index.ts
@@ -3,7 +3,7 @@ import type { Input } from "../utils/types";
 export default async function run({ inputLines }: Input) {
   const reports = inputLines.map((l) => l.split(" ").map((x) => +x));
 
-  const safe = reports.filter((r) => {
+  const isSafe = (r: number[]) => {
     let dir = 0;
     for (let i = 1; i < r.length; i++) {
       const diff = r[i] - r[i - 1];
@@ -17,33 +17,26 @@ export default async function run({ inputLines }: Input) {
       }
     }
     return true;
-  });
+  };
 
-  console.log(safe.length);
-
-  const safeWithDampener = reports.filter((r) => {
-    if (safe.includes(r)) {
-      return true;
+  const reportStatuses = reports.map((r) => {
+    if (isSafe(r)) {
+      return {
+        safe: true,
+        safeWithDampener: true,
+      };
     }
-    const options = r.map((_, i) => r.filter((_1, i2) => i !== i2));
-    const safeOptions = options.filter((x) => {
-      let dir = 0;
-      for (let i = 1; i < x.length; i++) {
-        const diff = x[i] - x[i - 1];
-        if (dir === 0) {
-          dir = diff;
-        } else if (diff * dir <= 0) {
-          return false;
-        }
-        if (diff === 0 || Math.abs(diff) > 3) {
-          return false;
-        }
-      }
-      return true;
-    });
 
-    return safeOptions.length > 0;
+    const options = r.map((_, i) => r.filter((_1, i2) => i !== i2));
+
+    return {
+      safe: false,
+      safeWithDampener: options.some(isSafe),
+    };
   });
 
-  console.log(safeWithDampener.length);
+  console.log(reportStatuses.filter(({ safe }) => safe).length);
+  console.log(
+    reportStatuses.filter(({ safeWithDampener }) => safeWithDampener).length
+  );
 }
