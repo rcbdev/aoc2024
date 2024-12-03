f3eed896ff363c82790007b7f14ec478c631c5e8
Rob Burgess <rob.burgess@live.co.uk>
2024-12-03 12:40:05 +0000

    day 3 inline regex

--- day3/index.ts
+++ day3/index.ts
@@ -5,16 +5,14 @@ export default async function run({ inputText }: Input) {
   const text = inputText.replaceAll("\n", "");
 
   const runInstructions = (text: string) => {
-    const regex = /mul\((\d+),(\d+)\)/g;
-    const matches = text.matchAll(regex);
+    const matches = text.matchAll(/mul\((\d+),(\d+)\)/g);
     const results = matches.map((m) => +m[1] * +m[2]).toArray();
     return sum(results);
   };
 
   console.log(runInstructions(text));
 
-  const disabledStuffRegex = /don't\(\).*?(do\(\)|$)/g;
-  const disabledText = text.matchAll(disabledStuffRegex);
+  const disabledText = text.matchAll(/don't\(\).*?(do\(\)|$)/g);
   const enabledText = disabledText.reduce(
     (rv, curr) => rv.replace(curr[0], ""),
     text
