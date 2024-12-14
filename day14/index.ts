e8bf5fa5d16e7a56f1969c82d956d105bcc2d115
Rob Burgess <rob.burgess@live.co.uk>
2024-12-14 14:25:01 +0000

    day 14 - even faster

--- day14/index.ts
+++ day14/index.ts
@@ -14,41 +14,39 @@ export default async function run({ inputLines }: Input) {
         .split(",")
         .map((x) => +x);
 
-      return { p, v, x: [] as number[], y: [] as number[] };
+      return {
+        p,
+        v,
+        x: [] as number[],
+        y: [] as number[],
+      };
     });
   const robots = getBots();
-  // const bounds = [11, 7];
   const bounds = [101, 103];
 
   const moveBots = (robots: ReturnType<typeof getBots>, amount = 1) => {
     robots.forEach((r) => {
       r.x.push(r.p[0]);
       r.y.push(r.p[1]);
-      r.p[0] = (r.p[0] + amount * r.v[0] + amount * bounds[0]) % bounds[0];
-      r.p[1] = (r.p[1] + amount * r.v[1] + amount * bounds[1]) % bounds[1];
+      r.p[0] = (r.p[0] + amount * (r.v[0] + bounds[0])) % bounds[0];
+      r.p[1] = (r.p[1] + amount * (r.v[1] + bounds[1])) % bounds[1];
     });
   };
 
   moveBots(robots, 100);
 
-  const quadrants = [];
-  for (let i = 0; i < 2; i++) {
-    for (let j = 0; j < 2; j++) {
-      const halves = [(bounds[0] + 1) / 2, (bounds[1] + 1) / 2];
-      const topLeft = [i * halves[0], j * halves[1]];
-      const bottomRight = [(i + 1) * halves[0] - 1, (j + 1) * halves[1] - 1];
-
-      const bots = robots.filter(
-        (r) =>
-          r.p[0] >= topLeft[0] &&
-          r.p[0] < bottomRight[0] &&
-          r.p[1] >= topLeft[1] &&
-          r.p[1] < bottomRight[1]
-      ).length;
-
-      quadrants.push(bots);
+  const halves = [(bounds[0] - 1) / 2, (bounds[1] - 1) / 2];
+  const quadrants = [0, 0, 0, 0];
+  robots.forEach((r) => {
+    const x = r.p[0] - halves[0];
+    const y = r.p[1] - halves[1];
+    if (x === 0 || y === 0) {
+      return;
     }
-  }
+    const index = (x < 0 ? 0 : 1) + (y < 0 ? 0 : 2);
+    quadrants[index]++;
+  });
+
   console.log(product(quadrants));
 
   const treeBots = getBots();
@@ -60,30 +58,34 @@ export default async function run({ inputLines }: Input) {
     moveBots(treeBots);
     i++;
     if (!xLoop) {
-      const looping = treeBots.filter((b) => b.x.includes(b.p[0]));
-      if (looping.length > treeBots.length / 2) {
-        const xs = looping
-          .map((r) => r.p[0])
-          .reduce(
-            (rv, curr) => ((rv[curr] = (rv[curr] ?? 0) + 1), rv),
-            {} as Record<number, number>
-          );
-        if (Object.values(xs).some((v) => v > 25)) {
-          xLoop = [i, looping[0].x.indexOf(looping[0].p[0])];
+      if (treeBots.every((b) => b.x.includes(b.p[0]))) {
+        for (let j = 0; j < treeBots[0].x.length; j++) {
+          const xs = treeBots
+            .map((r) => r.x[j])
+            .reduce(
+              (rv, curr) => ((rv[curr] = (rv[curr] ?? 0) + 1), rv),
+              {} as Record<number, number>
+            );
+          if (Object.values(xs).some((v) => v > 25)) {
+            xLoop = [i + j, j];
+            break;
+          }
         }
       }
     }
     if (!yLoop) {
-      const looping = treeBots.filter((b) => b.y.includes(b.p[1]));
-      if (looping.length > treeBots.length / 2) {
-        const ys = looping
-          .map((r) => r.p[1])
-          .reduce(
-            (rv, curr) => ((rv[curr] = (rv[curr] ?? 0) + 1), rv),
-            {} as Record<number, number>
-          );
-        if (Object.values(ys).some((v) => v > 25)) {
-          yLoop = [i, looping[0].y.indexOf(looping[0].p[1])];
+      if (treeBots.every((b) => b.y.includes(b.p[1]))) {
+        for (let j = 0; j < treeBots[0].y.length; j++) {
+          const ys = treeBots
+            .map((r) => r.y[j])
+            .reduce(
+              (rv, curr) => ((rv[curr] = (rv[curr] ?? 0) + 1), rv),
+              {} as Record<number, number>
+            );
+          if (Object.values(ys).some((v) => v > 25)) {
+            yLoop = [i + j, j];
+            break;
+          }
         }
       }
     }
