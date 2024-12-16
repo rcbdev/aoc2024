116d1670924ef6f96785d95ea6ea4bed2a8dd34f
Rob Burgess <rob.burgess@live.co.uk>
2024-12-16 08:19:17 +0000

    day 16 - separate part 1 and 2

--- day16/index.ts
+++ day16/index.ts
@@ -24,81 +24,89 @@ export default async function run({ inputLines }: Input) {
   );
   const coordString = (coord: number[]) => `${coord[0]}-${coord[1]}`;
 
-  const queue = new PriorityQueue<PathOption>((a, b) => a.score < b.score);
-  queue.push({
-    position: start,
-    last: null,
-    score: 0,
-    dir: "r",
-    visited: [],
-  });
-  const bestPaths = [];
-  let bestScore = Infinity;
+  const walkMap = (allRoutes = false) => {
+    const queue = new PriorityQueue<PathOption>((a, b) => a.score < b.score);
+    queue.push({
+      position: start,
+      last: null,
+      score: 0,
+      dir: "r",
+      visited: [],
+    });
+    const bestPaths = [];
+    let bestScore = Infinity;
 
-  const bestMap = map.map((l) =>
-    l.map<Record<string, number>>(() => ({
-      u: Infinity,
-      d: Infinity,
-      r: Infinity,
-      l: Infinity,
-    }))
-  );
+    const bestMap = map.map((l) =>
+      l.map<Record<string, number>>(() => ({
+        u: Infinity,
+        d: Infinity,
+        r: Infinity,
+        l: Infinity,
+      }))
+    );
 
-  while (!queue.isEmpty()) {
-    const current = queue.pop();
-    if (current.score > bestScore) {
-      break;
-    }
-    if (current.position[0] === end[0] && current.position[1] === end[1]) {
-      bestPaths.push(current);
-      bestScore = current.score;
-      continue;
-    }
-    if (
-      current.score >
-      bestMap[current.position[0]][current.position[1]][current.dir]
-    ) {
-      continue;
-    }
-    bestMap[current.position[0]][current.position[1]][current.dir] =
-      current.score;
-    const up = [current.position[0] - 1, current.position[1]];
-    const down = [current.position[0] + 1, current.position[1]];
-    const left = [current.position[0], current.position[1] - 1];
-    const right = [current.position[0], current.position[1] + 1];
-
-    const candidates = [
-      [up, "u"],
-      [down, "d"],
-      [left, "l"],
-      [right, "r"],
-    ] as [number[], string][];
-
-    for (const [candidate, dir] of candidates) {
+    while (!queue.isEmpty()) {
+      const current = queue.pop();
+      if (current.score > bestScore) {
+        break;
+      }
+      if (current.position[0] === end[0] && current.position[1] === end[1]) {
+        bestPaths.push(current);
+        bestScore = current.score;
+        continue;
+      }
       if (
-        current.last?.[0] === candidate[0] &&
-        current.last?.[1] === candidate[1]
+        current.score + (allRoutes ? 0 : 1) >
+        bestMap[current.position[0]][current.position[1]][current.dir]
       ) {
         continue;
       }
-      if (map[candidate[0]][candidate[1]] === "#") {
-        continue;
+      bestMap[current.position[0]][current.position[1]][current.dir] =
+        current.score;
+      const up = [current.position[0] - 1, current.position[1]];
+      const down = [current.position[0] + 1, current.position[1]];
+      const left = [current.position[0], current.position[1] - 1];
+      const right = [current.position[0], current.position[1] + 1];
+
+      const candidates = [
+        [up, "u"],
+        [down, "d"],
+        [left, "l"],
+        [right, "r"],
+      ] as [number[], string][];
+
+      for (const [candidate, dir] of candidates) {
+        if (
+          current.last?.[0] === candidate[0] &&
+          current.last?.[1] === candidate[1]
+        ) {
+          continue;
+        }
+        if (map[candidate[0]][candidate[1]] === "#") {
+          continue;
+        }
+        const scoreAdd = current.dir === dir ? 1 : 1001;
+        queue.push({
+          position: candidate,
+          last: current.position,
+          score: current.score + scoreAdd,
+          visited: [...current.visited, current.position],
+          dir,
+        });
       }
-      const scoreAdd = current.dir === dir ? 1 : 1001;
-      queue.push({
-        position: candidate,
-        last: current.position,
-        score: current.score + scoreAdd,
-        visited: [...current.visited, current.position],
-        dir,
-      });
     }
-  }
+    return {
+      paths: bestPaths,
+      score: bestScore,
+    };
+  };
 
-  console.log(bestScore);
+  const { score } = walkMap();
+  console.log(score);
 
+  const { paths } = walkMap(true);
   const totalVisited = new Set<string>();
-  bestPaths.forEach((path) =>
+  paths.forEach((path) =>
     path.visited.forEach((l) => totalVisited.add(coordString(l)))
   );
   console.log(totalVisited.size + 1);
