0740fa3d3e06772f42834413c00837a9df830c13
Rob Burgess <rob.burgess@live.co.uk>
2024-12-20 12:44:40 +0000

    day 20 - more perf improvement

--- day20/index.ts
+++ day20/index.ts
@@ -42,9 +42,9 @@ export default async function run({ inputLines }: Input) {
 
   const findCheats = (time = 2, cheatTarget = 100) => {
     return path.reduce((rv, x, i) => {
-      const possible = path.slice(i).filter((y, j) => {
+      const possible = path.slice(i + cheatTarget).filter((y, j) => {
         const dist = Math.abs(x[0] - y[0]) + Math.abs(x[1] - y[1]);
-        return j - dist >= cheatTarget && dist <= time;
+        return j >= dist && dist <= time;
       });
 
       return rv + possible.length;
