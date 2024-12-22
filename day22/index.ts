30b962f687590343624584dec7afe6620709c40b
Rob Burgess <rob.burgess@live.co.uk>
2024-12-22 18:39:25 +0000

    day 22 - simpler data structures, much quicker

--- day22/index.ts
+++ day22/index.ts
@@ -4,7 +4,7 @@ import type { Input } from "../utils/types";
 export default async function run({ inputLines }: Input) {
   const startingNumbers = inputLines.map((l) => +l);
 
-  const changeSequences = new Map<string, Map<number, number>>();
+  const changeSequences = new Map<number, number>();
 
   const sequencer = (secret: number) => {
     const mult64 = secret * 64;
@@ -16,25 +16,26 @@ export default async function run({ inputLines }: Input) {
     return step3;
   };
 
-  const runForSteps = (steps: number) => (number: number, idx: number) => {
+  const runForSteps = (steps: number) => (number: number) => {
     let num = number;
-    const diffs: number[] = [];
+    let diffs: number = 0;
+    const seen = new Set<number>();
     for (let i = 0; i < steps; i++) {
       const next = sequencer(num);
       const price = next % 10;
       const lastPrice = num % 10;
-      diffs.push(price - lastPrice);
-      if (diffs.length > 4) {
-        diffs.shift();
-      }
-      if (diffs.length === 4) {
-        const key = diffs.join();
-        if (!changeSequences.has(key)) {
-          changeSequences.set(key, new Map());
-        }
-        const map = changeSequences.get(key)!;
-        if (!map.has(idx)) {
-          map.set(idx, price);
+      const diff = price - lastPrice;
+      diffs = ((diffs << 5) + 10 + diff) % 1_048_576;
+      if (i >= 3) {
+        const key = diffs;
+        if (!seen.has(key)) {
+          if (!changeSequences.has(key)) {
+            changeSequences.set(key, price);
+          } else {
+            const total = changeSequences.get(key)!;
+            changeSequences.set(key, total + price);
+          }
+          seen.add(key);
         }
       }
       num = next;
@@ -45,10 +46,7 @@ export default async function run({ inputLines }: Input) {
   const after2000 = startingNumbers.map(runForSteps(2000));
   console.log(sum(after2000));
 
-  const sequenceValues = changeSequences
-    .entries()
-    .map(([k, v]) => [k, sum(v.values())] as const)
-    .toArray();
+  const sequenceValues = changeSequences.entries().toArray();
   const best = sequenceValues.sort((a, b) => b[1] - a[1])[0];
-  console.log(best);
+  console.log(best[1]);
 }
