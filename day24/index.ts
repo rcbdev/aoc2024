c73a2393482f8ded063c2e8689dd418b64dd2b97
Rob Burgess <rob.burgess@live.co.uk>
2024-12-24 09:21:30 +0000

    day 24 - output to graphviz and inspect

--- day24/index.ts
+++ day24/index.ts
@@ -11,6 +11,8 @@ export default async function run({ inputLines }: Input) {
   const wires: Record<string, number> = {};
   const gates: Gate[] = [];
   let largestZ = 0;
+  let largestX = 0;
+  let largestY = 0;
 
   let onWires = true;
   for (const line of inputLines) {
@@ -21,6 +23,18 @@ export default async function run({ inputLines }: Input) {
     if (onWires) {
       const [wire, value] = line.split(": ");
       wires[wire] = value === "1" ? 1 : 0;
+      if (wire.startsWith("x")) {
+        const number = +wire.substring(1);
+        if (number > largestX) {
+          largestX = number;
+        }
+      }
+      if (wire.startsWith("y")) {
+        const number = +wire.substring(1);
+        if (number > largestY) {
+          largestY = number;
+        }
+      }
     } else {
       const [in1, type, in2, _, out] = line.split(" ");
       const gate: Gate = {
@@ -38,6 +52,8 @@ export default async function run({ inputLines }: Input) {
       }
     }
   }
+
+  const originalWires = Object.keys(wires);
 
   const wireVal = (wireKey: string) => wires[wireKey];
   const wireActive = (wireKey: string) => wireKey in wires;
@@ -70,4 +86,39 @@ export default async function run({ inputLines }: Input) {
     result = result * 2 + wires[`z${i.toString().padStart(2, "0")}`];
   }
   console.log(result);
+
+  let x = 0;
+  for (let i = largestX; i >= 0; i--) {
+    x = x * 2 + wires[`x${i.toString().padStart(2, "0")}`];
+  }
+  let y = 0;
+  for (let i = largestY; i >= 0; i--) {
+    y = y * 2 + wires[`y${i.toString().padStart(2, "0")}`];
+  }
+  const actualSum = (x + y).toString(2);
+  const wrongSum = result.toString(2);
+  const wrong: string[] = [];
+  for (let i = 0; i < actualSum.length; i++) {
+    if (actualSum[i] !== wrongSum[i]) {
+      wrong.push(`z${i.toString().padStart(2, "0")}`);
+    }
+  }
+  console.log(wrong);
+
+  const graph: string[] = [];
+  const shapes: string[] = [];
+  for (const wire in wires) {
+    const theGates = gates.filter((g) => g.inputs.includes(wire));
+    for (const gate of theGates) {
+      graph.push(`${wire} -> ${gate.type}${gate.output};`);
+      graph.push(`${gate.type}${gate.output} -> ${gate.output};`);
+    }
+    if (wire.startsWith("z")) {
+      shapes.push(`${wire} [shape=Msquare];`);
+    }
+  }
+  Bun.write(
+    Bun.file("./day24/output.txt"),
+    graph.join("\n") + "\n\n" + shapes.join("\n")
+  );
 }
